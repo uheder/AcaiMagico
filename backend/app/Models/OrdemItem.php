9e48abc5c8020610816fbaf1266cc7ff90f51516
Uheder Macedo <dev.usmacedo@outlook.com>
2025-02-12 00:20:42 -0300

    feat: add models, controllers, factories, seeders

---
+++ backend/app/Models/OrdemItem.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrdemItem extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrdemItemFactory> */
+    use HasFactory;
+}
