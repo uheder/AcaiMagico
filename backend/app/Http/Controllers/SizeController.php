2976c000f6bc5d85fd1502538fdfe651bd60b3d0
Uheder Macedo <dev.usmacedo@outlook.com>
2025-02-12 05:09:48 -0300

    feat: add first size controllers, and api routes

--- backend/app/Http/Controllers/SizeController.php
+++ backend/app/Http/Controllers/SizeController.php
@@ -2,9 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Size;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
 
 class SizeController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        return response()->json(Size::all());
+    }
+
+    public function show(Size $size): JsonResponse
+    {
+        return response()->json(Size::find($size));
+    }
+
+    public function create(): View
+    {
+        return view('sizes.create');
+
+        // TODO: create form
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate
+        ([
+            'nome' => 'required | string | max:55| unique:sizes',
+            'valor' => 'required | decimal:0,2 | min:7',
+            'max_cremes' => 'required | integer| min:1',
+            'max_recheios' => 'required | integer | min:3',
+            'max_acompanhamentos' => 'required | integer | min:1',
+            'max_coberturas' => 'required | integer | min:1',
+        ]);
+
+        $size = new Size();
+        $size->nome = $request['nome'];
+        $size->valor = (float) $request['valor'];
+        $size->max_cremes = $request['max_cremes'];
+        $size->max_recheios = $request['max_recheios'];
+        $size->max_acompanhamentos = $request['max_acompanhamentos'];
+        $size->max_coberturas = $request['max_coberturas'];
+        $size->save();
+
+        return redirect()->route('sizes.index');
+    }
+
+    public function update(Request $request, int $id): JsonResponse
+    {
+        $request->validate([
+            'nome' => 'string | max:55 | unique:sizes, nome,',
+            'valor' => 'float | min:7',
+            'max_cremes' => 'integer | min:1',
+            'max_recheios' => 'integer| min:3',
+            'max_acompanhamentos' => 'integer | min:1',
+            'max_coberturas' => 'integer | min:1',
+        ]);
+
+        Size::find($id)->update($request->all());
+        return response()->json([
+            ['message'=>'Atualizado com sucesso!', 202],
+            Size::find($id)
+        ]);
+    }
+
+    public function delete(int $id)
+    {
+        Size::find($id)->delete();
+        return redirect()->route('sizes.index');
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,29 @@
 <?php
 
+use App\Http\Controllers\SizeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('v1')->group(function(){
+//    Route::prefix('ingredientes')->group(function () {
+//        Route::get('/', 'IngredientController@index')->name('ingredient.index');
+//        Route::get('/{id}', 'IngredientController@show')->name('ingredient.show');
+//        Route::get('/create', 'IngredientController@create')->name('ingredient.create');
+//        Route::post('/create', 'IngredientController@store')->name('ingredient.store');
+//        Route::delete('/{id}', 'IngredientController@destroy')->name('ingredient.delete');
+//    }); TODO - update controller
+
+    Route::prefix('tamanhos')->group(function () {
+        Route::get('/', [SizeController::class, 'index'])->name('sizes.index');
+        Route::get('/{id}', [SizeController::class, 'show'])->name('sizes.show');
+        Route::get('/criar', [SizeController::class, 'create'])->name('sizes.create');
+        Route::post('/criar', [SizeController::class , 'store'])->name('sizes.store');
+        Route::put('/update/{id}', [SizeController::class, 'update'])->name('sizes.update');
+        Route::delete('/delete/{id}', [SizeController::class, 'delete'])->name('sizes.delete');
+    });
 });
