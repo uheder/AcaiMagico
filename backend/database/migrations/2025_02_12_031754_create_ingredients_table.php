0e048f9fe5e2d0c611ec6f0c35a099dc3c0f3f21
Uheder Macedo <dev.usmacedo@outlook.com>
2025-02-12 23:15:30 -0300

    feat: add contraint max length for ingredients table column nome

--- backend/database/migrations/2025_02_12_031754_create_ingredients_table.php
+++ backend/database/migrations/2025_02_12_031754_create_ingredients_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('ingredients', function (Blueprint $table) {
             $table->id();
-            $table->string('nome');
+            $table->string('nome', 25);
             $table->string('categoria', 15);
             $table->timestamps();
         });
