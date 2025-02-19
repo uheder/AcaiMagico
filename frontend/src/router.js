f2c78363e517dfed55e42168d9c002a579751096
Uheder Macedo <dev.usmacedo@outlook.com>
2025-02-19 12:53:22 -0300

    feat: add first routes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,32 +2,34 @@ import DefaultLayout from "./components/DefaultLayout.vue";
 import Home from "./components/Pages/Delivery/Home.vue";
 import Cart from "./components/Pages/Delivery/Cart.vue";
 import Login from "./components/Pages/Dashboards/Login.vue";
-import Register from "./components/Pages/Dashboards/Register.vue";
-import DashboardAdmin from "./components/Pages/Dashboards/DashboardAdmin.vue";
-import Orders from "./components/Pages/Dashboards/Orders.vue";
-import Deliveries from "./components/Pages/Dashboards/Deliveries.vue";
-import Ingredients from "./components/Pages/Dashboards/Ingredients.vue";
-import Sizes from "./components/Pages/Dashboards/Sizes.vue";
+import UserRegister from "./components/Pages/Dashboards/Admin/UserRegister.vue";
+import Orders from "./components/Pages/Dashboards/Admin/Orders.vue";
+import Deliveries from "./components/Pages/Dashboards/Admin/Deliveries.vue";
+import Ingredients from "./components/Pages/Dashboards/Admin/Ingredients.vue";
+import Sizes from "./components/Pages/Dashboards/Admin/Sizes.vue";
 import {createRouter, createWebHistory} from "vue-router";
+import Dashboard from "./components/Pages/Dashboards/Admin/Dashboard.vue";
 
-const routes = [ {
-    // delivery routes
-    path: '/',
-    component: DefaultLayout,
-    children: [
-        { path: '/', name: 'Home', component: Home },
-        { path: '/cart', name: 'Cart', component: Cart },
+const routes = [
+    {
+        path: '/',
+        component: DefaultLayout,
+        children: [
+            {path: '/', name: 'Home', component: Home},
+            {path: '/cart', name: 'Cart', component: Cart},
         ]
     },
     {
-        path: '/dashboard',
-        component: DashboardAdmin,
-        name: 'Dashboard'
-    },
-    {
-        path: '/register',
-        component: Register,
-        name: 'Register'
+        path: '/admin',
+        component: DefaultLayout,
+        children: [
+            {path: '', name: 'Dashboard', component: Dashboard },
+            {path: 'orders', name: 'Orders', component: Orders},
+            {path: 'deliveries', name: 'Deliveries', component: Deliveries},
+            {path: 'ingredients', name: 'Ingredients', component: Ingredients},
+            {path: 'sizes', name: 'Sizes', component: Sizes},
+            {path: 'register', name: 'UserRegister', component: UserRegister},
+        ]
     },
     {
         path: '/login',
@@ -35,26 +37,7 @@ const routes = [ {
         name: 'Login'
     },
     {
-        path: '/orders',
-        component: Orders,
-        name: 'Orders'
-    },
-    {
-        path: '/deliveries',
-        component: Deliveries,
-        name: 'Deliveries'
-    },
-    {
-      path: '/ingredients',
-      component: Ingredients,
-      name: 'Ingredients'
-    },
-    {
-      path: '/sizes',
-      component: Sizes,
-      name: 'Sizes'
-    },
-    {
+        // deals with non set routes
         path: '/:catchAll(.*)*',
         redirect: '/'
     }
