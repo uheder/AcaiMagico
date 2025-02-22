bf8b539f42d70a16e3fe3aeb247c5567c000bbec
Uheder Macedo <dev.usmacedo@outlook.com>
2025-02-22 04:56:14 -0300

    feat: first tries at admin dashboards

--- frontend/src/components/Pages/Dashboards/Admin/Orders.vue
+++ frontend/src/components/Pages/Dashboards/Admin/Orders.vue
@@ -1,5 +1,11 @@
 <script setup>
 import DefaultAdminLayout from "../../../DefaultAdminLayout.vue";
+
+import {getPedidos} from "../../../../api.js";
+import {onMounted, ref} from "vue";
+
+const pedidos = ref([]);
+onMounted(() => getPedidos(pedidos))
 </script>
 
 <template>
@@ -10,8 +16,30 @@ import DefaultAdminLayout from "../../../DefaultAdminLayout.vue";
       </div>
     </header>
     <main>
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <strong>Aqui serão listados os pedidos</strong>
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg: ml-4 mr-4">
+        <div v-for="pedido in pedidos"
+             class="text-sm/4 inline-flex items-center">
+          <ul class="list-disc">
+            <hr>
+            <li>Número do pedido: {{ pedido.numero }}</li>
+            <li>Nome do cliente: {{pedido.nome_cliente }}</li>
+            <li>Observação: {{ pedido.observacao }}</li>
+            <li>Status: {{ pedido.status }}</li>
+            <li>Tipo de entrega: {{ pedido.tipo_entrega }}</li>
+            <li>Total do pedido: R$ {{ pedido.total }}</li>
+            <li>Itens do pedido:</li>
+            <ul v-for="item in pedido.items">
+              <li>Tamanho: {{ item.tamanho }}</li>
+              <li>Cremes: {{ item.creme ? item.creme : "Sem"}}</li>
+              <li>Recheios: {{ item.recheio ? item.recheio : "Sem"}}</li>
+              <li>Acompanhamentos: {{ item.acompanhamento ? item.acompanhamento : "Sem"}}</li>
+              <li>Coberturas: {{ item.cobertura ? item.cobertura : "Sem"}}</li>
+              <li>Quantidade: {{ item.quantidade }}</li>
+              <hr class="border-gray-900/10">
+            </ul>
+              <li>Informações de entrega: {{ pedido.tipo_entrega === "Entrega" ? pedido.entrega : ""}}</li>
+          </ul>
+        </div>
       </div>
     </main>
 
--- frontend/src/components/Pages/Dashboards/Admin/UserRegister.vue
+++ frontend/src/components/Pages/Dashboards/Admin/UserRegister.vue
@@ -47,16 +47,6 @@ import DefaultAdminLayout from "../../../DefaultAdminLayout.vue";
               <input type="password" name="repeat_password" id="repeat_password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
             </div>
           </div>
-          <!-- ADMIN PASSWORD -->
-          <div>
-            <div class="flex items-center justify-between">
-              <label for="password" class="block text-sm/6 font-medium text-gray-900">Senha do Admin</label>
-            </div>
-            <div class="mt-2">
-              <input type="password" name="repeat_password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-            </div>
-          </div>
-
           <div>
             <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Cadastrar Usuário</button>
           </div>
