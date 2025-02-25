4504602a38e8a281f327610d8866f98e8fef7bf1
Uheder Macedo <dev.usmacedo@outlook.com>
2025-02-25 01:32:28 -0300

    feat: add pinia uses

---
+++ frontend/src/store/acompanhamento.js
@@ -0,0 +1,45 @@
+import {defineStore} from "pinia";
+import axiosClient from "../axios.js";
+
+const useAcompanhamentoStore = defineStore('acompanhamento', {
+    state: () => ({
+        acompanhamentos: []
+    }),
+    actions: {
+        async fetchAcompanhamentos() {
+            try {
+                const response = await axiosClient.get('/api/acompanhamentos');
+                this.acompanhamentos = Object.values(response.data).map(acompanhamento => ({
+                    id: acompanhamento["id"],
+                    name: acompanhamento["nome"],
+                    status: acompanhamento["status"],
+                }))
+            } catch (error) {
+                console.error(error);
+            }
+        },
+
+        async update_acompanhamento(acompanhamento) {
+            try {
+                const response = await axiosClient.put(`/api/acompanhamentos/${acompanhamento.id}`, {
+                    status: acompanhamento.status === 'ativo' ? 'inativo' : 'ativo'
+                });
+                const acompanhamentoData = response.data;
+                this.acompanhamentos = this.acompanhamentos.map(c => c.id === acompanhamento.id ? {...c, status: acompanhamentoData.status} : c);
+            } catch (error) {
+                console.error(error);
+            }
+        },
+
+        async delete_acompanhamento(acompanhamento) {
+            try {
+                await axiosClient.delete(`/api/acompanhamentos/${acompanhamento.id}`);
+                this.acompanhamentos = this.acompanhamentos.filter(c => c.id !== acompanhamento.id);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
+})
+
+export default useAcompanhamentoStore;
