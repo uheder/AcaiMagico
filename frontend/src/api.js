cf1498e08946a10b91b264b5d74717985201146c
Uheder Macedo <dev.usmacedo@outlook.com>
2025-02-25 01:34:21 -0300

    fix: remove some unused keys for now

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -16,22 +16,35 @@ async function getTamanhos(tamanhos) {
 // Those functions will receive empty arrays, then fill them with the info received by the API
 async function getCremes(cremes) {
     const response = await axiosClient.get('/api/cremes');
-    cremes.value = Object.values(response.data).map(creme => ({name: creme["nome"]}));
+    cremes.value = Object.values(response.data).map(creme => ({
+        id: creme["id"],
+        name: creme["nome"],
+        status: creme["status"],
+    }));
 }
 
 async function getRecheios(recheios) {
     const response = await axiosClient.get('/api/recheios');
-    recheios.value = Object.values(response.data).map(recheio => ({name: recheio["nome"]}));
+    recheios.value = Object.values(response.data).map(recheio => ({
+        name: recheio["nome"],
+        status: recheio["status"],
+    }));
 }
 
 async function getAcompanhamentos(acompanhamentos) {
     const response = await axiosClient.get('/api/acompanhamentos');
-    acompanhamentos.value = Object.values(response.data).map(acompanhamento => ({name: acompanhamento["nome"]}));
+    acompanhamentos.value = Object.values(response.data).map(acompanhamento => ({
+        name: acompanhamento["nome"],
+        status: acompanhamento["status"],
+    }));
 }
 
 async function getCoberturas(coberturas) {
     const response = await axiosClient.get('/api/coberturas');
-    coberturas.value = Object.values(response.data).map(cobertura => ({name: cobertura["nome"]}));
+    coberturas.value = Object.values(response.data).map(cobertura => ({
+        name: cobertura["nome"],
+        status: cobertura["status"],
+    }));
 }
 
 
@@ -44,16 +57,26 @@ async function getPedidos(pedidos) {
         status: pedido['status'],
         tipo_entrega: pedido['tipo_entrega'],
         total: pedido['total'],
-        items: pedido.items.map(item => ({
-            tamanho: item['size_id'],
-            quantidade: item['quantidade'],
-            creme: item['creme_id'],
-            recheio: item['recheio_id'],
-            acompanhamento: item['acompanhamento_id'],
-            cobertura: item['cobertura_id'],
-        })),
-        entrega: pedido['delivery'],
     }));
 }
 
-export {getTamanhos, getCremes, getRecheios, getAcompanhamentos, getCoberturas, getPedidos};
+async function getEntregas(entregas) {
+    const response = await axiosClient.get('/api/deliveries');
+    entregas.value = Object.values(response.data).map(entrega => ({
+        pedido: entrega['id'],
+        nome_cliente: entrega['delivery']['nome_cliente'],
+        telefone_cliente: entrega['delivery']['telefone_cliente'],
+        rua: entrega['delivery']['rua'],
+        bairro: entrega['delivery']['bairro'],
+        numero: entrega['delivery']['numero'],
+        referencia: entrega['delivery']['referencia'],
+        forma_pagamento: entrega['delivery']['forma_pagamento'],
+        troco: entrega['delivery']['troco'] ? 'Sim' : 'Não',
+        troco_para: entrega['delivery']['troco'] ? entrega['delivery']['troco_para'] : 0,
+    }));
+}
+
+
+
+
+export {getTamanhos, getCremes, getRecheios, getAcompanhamentos, getCoberturas, getPedidos, getEntregas};
