a86e2d3f9721b3f2c963d614bc935bb196f7dbee
Uheder Macedo <dev.usmacedo@outlook.com>
2025-02-27 00:37:35 -0300

    feat: add data from the api

--- frontend/src/components/Pages/Dashboards/Admin/Sizes.vue
+++ frontend/src/components/Pages/Dashboards/Admin/Sizes.vue
@@ -1,6 +1,14 @@
 <script setup>
 
 import DefaultAdminLayout from "../../../DefaultAdminLayout.vue";
+import useSizeStore from "../../../../store/sizes.js";
+import {onMounted} from "vue";
+
+const sizeStore = useSizeStore();
+
+onMounted(() => {
+  sizeStore.fetchSizes();
+});
 </script>
 
 <template>
@@ -12,7 +20,23 @@ import DefaultAdminLayout from "../../../DefaultAdminLayout.vue";
     </header>
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <strong>Aqui serão listados os tamanhos</strong>
+        <div class="mx-auto max-w-7xl px-1 py-2 m-0 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div v-for="tamanho in sizeStore.sizes" class="text-sm/4 px-2">
+              <div class="my-2 bg-white shadow-sm border border-slate-500 rounded-lg">
+                <div class="p-4 bg-purple-400">
+                  <ul>
+                    <li>{{ tamanho.nome }}</li>
+                    <li>Valor: R${{ tamanho.valor }}</li>
+                    <li>Max. Cremes: {{ tamanho.qtd_cremes}}</li>
+                    <li>Max. Recheios: {{ tamanho.qtd_recheios}}</li>
+                    <li>Max. Acompanhamentos: {{ tamanho.qtd_acompanhamentos}}</li>
+                    <li>Max. Coberturas: {{ tamanho.qtd_coberturas}}</li>
+                    <li>Status: {{ tamanho.status.toUpperCase() }}</li>
+                  </ul>
+                </div>
+              </div>
+          </div>
+        </div>
       </div>
     </main>
 
