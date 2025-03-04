1d54089b5bd16affd794978afc23da8a207e8446
Uheder Macedo <dev.usmacedo@outlook.com>
2025-03-04 01:22:32 -0300

    fix: labels, and add some responsitivity

--- frontend/src/components/Pages/Delivery/Home.vue
+++ frontend/src/components/Pages/Delivery/Home.vue
@@ -16,15 +16,15 @@ const recheioStore = useRecheioStore();
 const acompanhamentoStore = useAcompanhamentoStore();
 const coberturaStore = useCoberturaStore();
 
-onMounted( () => {
-    sizeStore.fetchSizes();
-    cremeStore.fetchCremes();
-    recheioStore.fetchRecheios();
-    acompanhamentoStore.fetchAcompanhamentos();
-    coberturaStore.fetchCoberturas();
+onMounted(() => {
+  sizeStore.fetchSizes();
+  cremeStore.fetchCremes();
+  recheioStore.fetchRecheios();
+  acompanhamentoStore.fetchAcompanhamentos();
+  coberturaStore.fetchCoberturas();
 })
 
-const data = ref ({
+const data = ref({
   tamanho: '',
   creme: [],
   recheio: [],
@@ -49,166 +49,167 @@ const submit = () => {
 
 <template>
   <div class="sm:max-w-full md:max-w-[90%]  lg:w-[75%] xl:max-w-[60%] mx-auto items-center justify-center bg-white shadow-md rounded-lg">
-  <form @submit.prevent="submit()">
-  <div class="space-y-12 ml-4 mr-4">
-    <!-- TAMANHOS -->
-    <div class="mt-4 space-y-10">
-      <div class="border-b border-gray-900/10 pb-4">
-        <h1 class="text-3xl font-semibold text-purple-900 text-center">Tamanhos</h1>
-        <fieldset>
-          <div class="grid grid-cols-4 gap-4 h-auto">
-            <div v-for="tamanho in sizeStore.sizes" :key="tamanho.id"
-                 class="text-sm/4 inline-flex items-center">
-              <input
-                  :id="`tamanho-${tamanho.id}`"
-                  type="radio"
-                  v-model="data.tamanho"
-                  :name="tamanho.id"
-                  :value="tamanho"
-                  class="relative size-4 appearance-none rounded-full border border-gray-300 bg-white before:absolute before:inset-1 before:rounded-full before:bg-white not-checked:before:hidden checked:border-indigo-600 checked:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400 forced-colors:appearance-auto forced-colors:before:hidden"
-              />
-              <div class="text-sm/4 ml-2">
-                <label :for="`creme-${tamanho.id}`" class="font-medium text-gray-900">
-                  {{ tamanho.name }}: R${{ tamanho.valor }}
-                </label>
+    <form @submit.prevent="submit()">
+      <div class="space-y-12 ml-4 mr-4">
+        <!-- TAMANHOS -->
+        <div class="mt-4 space-y-10">
+          <div class="border-b border-gray-900/10 pb-4">
+            <h1 class="text-3xl font-semibold text-purple-900 text-center">Tamanhos</h1>
+            <fieldset>
+              <div class="grid grid-cols-4 gap-4 h-auto">
+                <div v-for="tamanho in sizeStore.sizes" :key="tamanho.id"
+                     class="text-sm/4 inline-flex items-center">
+                  <input
+                      :id="`tamanho-${tamanho.id}`"
+                      type="radio"
+                      v-model="data.tamanho"
+                      :name="tamanho.id"
+                      :value="tamanho"
+                      class="relative size-4 appearance-none rounded-full border border-gray-300 bg-white before:absolute before:inset-1 before:rounded-full before:bg-white not-checked:before:hidden checked:border-indigo-600 checked:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400 forced-colors:appearance-auto forced-colors:before:hidden"
+                  />
+                  <div class="text-sm/4 ml-2">
+                    <label :for="`tamanho-${tamanho.id}`" class="font-medium text-gray-900">
+                      {{ tamanho.name }}: R${{ tamanho.valor }}
+                    </label>
+                  </div>
+                </div>
               </div>
-            </div>
+            </fieldset>
           </div>
-        </fieldset>
         </div>
-    </div>
 
-    <!-- CREMES -->
-    <div class="mt-4 space-y-10">
-      <div class="border-b border-gray-900/10 pb-4">
-        <h1 class="text-3xl font-semibold text-purple-900 text-center">Cremes</h1>
-        <fieldset>
-          <div class="grid grid-cols-4 gap-4 h-auto auto-rows-auto">
-            <div v-for="creme in cremeStore.cremes" :key="creme.id"
-                 class="text-sm/4 inline-flex items-center">
-              <input
-                  :id="`creme-${creme.id}`"
-                  type="checkbox"
-                  v-model="data.creme"
-                  :value="creme"
-                  :class="{
+        <!-- CREMES -->
+        <div class="mt-4 space-y-10">
+          <div class="border-b border-gray-900/10 pb-4">
+            <h1 class="text-3xl font-semibold text-purple-900 text-center">Cremes</h1>
+            <fieldset>
+              <div class="grid grid-cols-3 lg:grid-cols-4 gap-4 h-auto auto-rows-auto">
+                <div v-for="creme in cremeStore.cremes" :key="creme.id"
+                     class="text-sm/4 inline-flex items-center">
+                  <input
+                      :id="`creme-${creme.id}`"
+                      type="checkbox"
+                      v-model="data.creme"
+                      :value="creme"
+                      :class="{
                     'col-start-1 row-start-1 rounded-sm border border-gray-300': true,
                     'bg-white checked:border-indigo-600 checked:bg-indigo-600': !creme.disabled,
                     'disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100': creme.disabled,
                   }"
-                  :disabled="creme.disabled"
-              />
-              <div class="text-sm/4 ml-2">
-                <label :for="`creme-${creme.id}`" class="font-medium text-gray-900">
-                  {{ creme.name }}
-                </label>
+                      :disabled="creme.disabled"
+                  />
+                  <div class="text-sm/4 ml-2">
+                    <label :for="`creme-${creme.id}`" class="font-medium text-gray-900">
+                      {{ creme.name }}
+                    </label>
+                  </div>
+                </div>
               </div>
-            </div>
+            </fieldset>
           </div>
-        </fieldset>
-      </div>
-    </div>
+        </div>
 
-    <!-- RECHEIOS -->
-    <div class="mt-10 space-y-10">
-      <div class="border-b border-gray-900/10 pb-4">
-        <h1 class="text-3xl font-semibold text-purple-900 text-center">Recheios</h1>
-        <fieldset>
-          <div class="grid grid-cols-4 gap-4 h-auto">
-            <div v-for="recheio in recheioStore.recheios" :key="recheio.id"
-                 class="text-sm/4 inline-flex items-center">
-              <input
-                  :id="`recheio-${recheio.id}`"
-                  type="checkbox"
-                  v-model="data.recheio"
-                  :value="recheio"
-                  :class="{
+        <!-- RECHEIOS -->
+        <div class="mt-10 space-y-10">
+          <div class="border-b border-gray-900/10 pb-4">
+            <h1 class="text-3xl font-semibold text-purple-900 text-center">Recheios</h1>
+            <fieldset>
+              <div class="grid grid-cols-3 lg:grid-cols-4 gap-4 h-auto">
+                <div v-for="recheio in recheioStore.recheios" :key="recheio.id"
+                     class="text-sm/4 inline-flex items-center">
+                  <input
+                      :id="`recheio-${recheio.id}`"
+                      type="checkbox"
+                      v-model="data.recheio"
+                      :value="recheio"
+                      :class="{
                     'col-start-1 row-start-1 rounded-sm border border-gray-300': true,
                     'bg-white checked:border-indigo-600 checked:bg-indigo-600': !recheio.disabled,
                     'disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100': recheio.disabled,
                   }"
-                  :disabled="recheio.disabled"
-              />
-              <div class="text-sm/4 ml-2">
-                <label :for="`creme-${recheio.id}`" class="font-medium text-gray-900">
-                  {{ recheio.name }}
-                </label>
+                      :disabled="recheio.disabled"
+                  />
+                  <div class="text-sm/4 ml-2">
+                    <label :for="`recheio-${recheio.id}`" class="font-medium text-gray-900">
+                      {{ recheio.name }}
+                    </label>
+                  </div>
+                </div>
               </div>
-            </div>
+            </fieldset>
           </div>
-        </fieldset>
         </div>
-    </div>
 
-    <!-- ACOMPANHAMENTOS -->
-    <div class="mt-10 space-y-5">
-      <div class="border-b border-gray-900/10 pb-4">
-        <h1 class="text-3xl font-semibold text-purple-900 text-center">Acompanhamentos</h1>
-        <fieldset>
-          <div class="grid grid-cols-4 gap-4 h-auto">
-            <div v-for="acompanhamento in acompanhamentoStore.acompanhamentos" :key="acompanhamento.id"
-                 class="text-sm/4 inline-flex items-center">
-              <input
-                  :id="`acompanhamento-${acompanhamento.id}`"
-                  type="checkbox"
-                  v-model="data.acompanhamento"
-                  :value="acompanhamento"
-                  :class="{
+        <!-- ACOMPANHAMENTOS -->
+        <div class="mt-10 space-y-5">
+          <div class="border-b border-gray-900/10 pb-4">
+            <h1 class="text-3xl font-semibold text-purple-900 text-center">Acompanhamentos</h1>
+            <fieldset>
+              <div class=" grid grid-cols-3 lg:grid-cols-4 gap-4 h-auto">
+                <div v-for="acompanhamento in acompanhamentoStore.acompanhamentos" :key="acompanhamento.id"
+                     class="text-sm/4 inline-flex items-center">
+                  <input
+                      :id="`acompanhamento-${acompanhamento.id}`"
+                      type="checkbox"
+                      v-model="data.acompanhamento"
+                      :value="acompanhamento"
+                      :class="{
                     'col-start-1 row-start-1 rounded-sm border border-gray-300': true,
                     'bg-white checked:border-indigo-600 checked:bg-indigo-600': !acompanhamento.disabled,
                     'disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100': acompanhamento.disabled,
                   }"
-                  :disabled="acompanhamento.disabled"
-              />
-              <div class="text-sm/4 ml-2">
-                <label :for="`creme-${acompanhamento.id}`" class="font-medium text-gray-900">
-                  {{ acompanhamento.name }}
-                </label>
+                      :disabled="acompanhamento.disabled"
+                  />
+                  <div class="text-sm/4 ml-2">
+                    <label :for="`acompanhamento-${acompanhamento.id}`" class="font-medium text-gray-900">
+                      {{ acompanhamento.name }}
+                    </label>
+                  </div>
+                </div>
               </div>
-            </div>
+            </fieldset>
           </div>
-        </fieldset>
-      </div>
-    </div>
+        </div>
 
-    <!-- COBERTURAS -->
-    <div class="mt-10 space-y-5">
-      <div class="border-b border-gray-900/10 pb-4">
-        <h1 class="text-3xl font-semibold text-purple-900 text-center">Coberturas</h1>
-        <fieldset>
-          <div class="grid grid-cols-4 gap-4 h-auto">
-            <div v-for="cobertura in coberturaStore.coberturas" :key="cobertura.id"
-                 class="text-sm/4 inline-flex items-center">
-              <input
-                  :id="`cobertura-${cobertura.id}`"
-                  type="checkbox"
-                  v-model="data.cobertura"
-                  :value="cobertura"
-                  :class="{
+        <!-- COBERTURAS -->
+        <div class="mt-10 space-y-5">
+          <div class="border-b border-gray-900/10 pb-4">
+            <h1 class="text-3xl font-semibold text-purple-900 text-center">Coberturas</h1>
+            <fieldset>
+              <div class="grid grid-cols-3 lg:grid-cols-4 gap-4 h-auto">
+                <div v-for="cobertura in coberturaStore.coberturas" :key="cobertura.id"
+                     class="text-sm/4 inline-flex items-center">
+                  <input
+                      :id="`cobertura-${cobertura.id}`"
+                      type="checkbox"
+                      v-model="data.cobertura"
+                      :value="cobertura"
+                      :class="{
                     'col-start-1 row-start-1 rounded-sm border border-gray-300': true,
                     'bg-white checked:border-indigo-600 checked:bg-indigo-600': !cobertura.disabled,
                     'disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100': cobertura.disabled,
                   }"
-                  :disabled="cobertura.disabled"
-              />
-              <div class="text-sm/4 ml-2">
-                <label :for="`creme-${cobertura.id}`" class="font-medium text-gray-900">
-                  {{ cobertura.name }}
-                </label>
+                      :disabled="cobertura.disabled"
+                  />
+                  <div class="text-sm/4 ml-2">
+                    <label :for="`cobertura-${cobertura.id}`" class="font-medium text-gray-900">
+                      {{ cobertura.name }}
+                    </label>
+                  </div>
+                </div>
               </div>
-            </div>
+            </fieldset>
           </div>
-        </fieldset>
+        </div>
       </div>
-    </div>
-  </div>
-  <div class="mt-4 flex items-center justify-center gap-x-6 pb-2">
-    <button class="text-center rounded-md w-[50%] mt-6 bg-purple-900 py-2 px-4 border border-transparent text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-cyan-700 focus:shadow-none active:bg-cyan-700 hover:bg-purple-950 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="submit">
-      Adicionar ao carrinho
-      <img src="../../../../public/svgCart.svg" class="w-6 h-6 justify-end inline-flex" alt="Adicionar ao carrinho">
-    </button>
-  </div>
-  </form>
+      <div class="mt-4 flex items-center justify-center gap-x-6 pb-2">
+        <button class="text-center rounded-md w-[50%] mt-6 bg-purple-900 py-2 px-4 border border-transparent text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-cyan-700 focus:shadow-none active:bg-cyan-700 hover:bg-purple-950 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="submit">
+          Adicionar ao carrinho
+          <img src="../../../../public/svgCart.svg" class="w-6 h-6 justify-end inline-flex" alt="Adicionar ao carrinho">
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
