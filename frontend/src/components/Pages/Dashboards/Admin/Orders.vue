93cada0bbfdd82d235afbf922f6a12811b0488dd
Uheder Macedo <dev.usmacedo@outlook.com>
2025-03-08 14:37:34 -0300

    feat: modifies visuals for admin order page

--- frontend/src/components/Pages/Dashboards/Admin/Orders.vue
+++ frontend/src/components/Pages/Dashboards/Admin/Orders.vue
@@ -12,9 +12,9 @@ onMounted(() => {
 
 <template>
   <DefaultAdminLayout>
-    <header class="shadow-sm">
-      <div class="mx-auto max-w-xl sm:px-6 lg:px-8 ">
-        <h1 class=" pl-3 text-3xl font-bold tracking-tight text-gray-900">Pedidos</h1>
+    <header class="bg-white shadow-sm">
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 ">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Pedidos</h1>
       </div>
     </header>
     <main>
