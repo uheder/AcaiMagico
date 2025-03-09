b19f7ac5b8163fa36d288ed7e2202a9fc36d4eae
Uheder Macedo <dev.usmacedo@outlook.com>
2025-03-09 02:27:24 -0300

    update: changes store to save all data from the endpoint

--- frontend/src/store/delivery.js
+++ frontend/src/store/delivery.js
@@ -9,20 +9,10 @@ const useDeliveryStore = defineStore('delivery', {
         async fetchDeliveries() {
             try {
                 const response = await axiosClient.get('/api/deliveries');
-                this.deliveries = Object.values(response.data).map(delivery => ({
-                    pedido: delivery['id'],
-                    nome_cliente: delivery['delivery']['nome_cliente'],
-                    telefone_cliente: delivery['delivery']['telefone_cliente'],
-                    rua: delivery['delivery']['rua'],
-                    bairro: delivery['delivery']['bairro'],
-                    numero: delivery['delivery']['numero'],
-                    referencia: delivery['delivery']['referencia'],
-                    forma_pagamento: delivery['delivery']['forma_pagamento'],
-                    troco: delivery['delivery']['troco'] ? 'Sim' : 'Não',
-                    troco_para: delivery['delivery']['troco'] ? delivery['delivery']['troco_para'] : 0,
-                }));
+                this.deliveries = response.data
             } catch (error) {
-                console.error(error);
+                console.error('Erro ao carregar entregas',error);
+                throw error;
             }
         },
 
@@ -34,7 +24,8 @@ const useDeliveryStore = defineStore('delivery', {
                 const deliveryData = response.data;
                 this.deliveries = this.deliveries.map(d => d.id === delivery.id ? {...d, status: deliveryData.status} : d);
             } catch (error) {
-                console.error(error);
+                console.error('Erro ao atualizar status da entrega', error);
+                throw error;
             }
         },
     }
